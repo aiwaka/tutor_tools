f503621673b7fd18b697cf3b47a2ec9b4cf12b7a
Aiwaka <Kw.I.KU.1414@gmail.com>
2021-04-19 02:39:47 +0900

    add settings page

---
+++ src/views/Settings.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="settings">
+      <form>
+          <label for="clear-data">全てのデータを消去</label>
+          <button id="clear-data" v-on:click.prevent="clearAllData">消去</button>
+      </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'settings',
+  data: () => {
+    return {
+    }
+  },
+  computed: {
+  },
+  methods: {
+    clearAllData() {
+      this.$store.dispatch('clearAllData')
+    }
+  }
+}
+</script>
+
+<style scoped>
+label {
+  display: inline-block;
+  margin-right: .5rem;
+}
+</style>
