c95b0f6396385c79465e09131341c858cc34448c
Aiwaka <Kw.I.KU.1414@gmail.com>
2021-04-18 23:57:34 +0900

    add state and getters

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,40 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    timeData: [],
+    nextTimeDataId: 0,
+    clientData: [],
+    currentClientId: -1,
+    nextClientId: 0,
+    year: 2021,
+    month: 1,
+  },
+  getters: {
+    getYear(state) {
+      return state.year
+    },
+    getMonth(state) {
+      return state.month
+    },
+    getNextTimeDataId(state) {
+      return state.nextTimeDataId
+    },
+    getClientData(state) {
+      return state.clientData
+    },
+    getCurrentClientId(state) {
+      return state.currentClientId
+    },
+    getCurrentClientData(state) {
+      const index = state.clientData.findIndex((data) => data.id === state.currentClientId)
+      return state.clientData[index]
+    }
   },
   mutations: {
+
   },
   actions: {
+
   },
   modules: {
   }
