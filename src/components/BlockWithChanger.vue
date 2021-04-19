c698d5467dcdbdd3f15ce0dce4543850a3fab6aa
Aiwaka <Kw.I.KU.1414@gmail.com>
2021-04-19 18:59:34 +0900

    add label props and modify css

--- src/components/BlockWithChanger.vue
+++ src/components/BlockWithChanger.vue
@@ -1,6 +1,7 @@
 <template>
     <!--textを受け取り表示する. changeボタンを押すと編集でき, saveを押すとtext-changedをemitし変更後のデータを返す. オプションでinputTypeを指定するとテキストのバリデーションを設定できる.-->
     <div class="block-with-changer">
+        <div class="label" v-if="label">{{label}}</div>
         <div class="text-space" v-if="show">{{text}}</div>
         <button v-if="show" v-on:click.prevent="change">Change</button>
         <input class="text-input" :type="inputType" v-if="!show" v-model="newText" />
@@ -18,6 +19,10 @@ export default {
         }
     },
     props: {
+        label: {
+            type: String,
+            default: ''
+        },
         text: {
             default: ''
         },
@@ -40,23 +45,26 @@ export default {
 </script>
 
 <style scoped>
+.label {
+    font-weight: bold;
+}
 .block-with-changer {
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     align-items: center;
-    height: 11rem;
+    height: 10rem;
 }
 .text-space {
-    width: 5em;
+    width: 10rem;
     word-wrap: break-word;
     margin: auto
 }
 .text-input {
-    width: 5em;
+    width: 10rem;
     margin: auto;
 }
 button {
-    margin: .6rem 0;
+    margin: .4rem 0;
 }
 </style>
