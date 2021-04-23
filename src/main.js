4b4925ee07b2ee2cf880841db51635d4d75509e8
Aiwaka <Kw.I.KU.1414@gmail.com>
2021-04-24 03:13:52 +0900

    add vuetify

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import vuetify from "./vuetify"
 import store from './store'
 
 Vue.config.productionTip = false
@@ -8,6 +9,7 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App),
   created() {
     store.dispatch('loadAll')
