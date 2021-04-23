9513ef212657d75d6310151aa6a2246dccbf1851
Aiwaka <Kw.I.KU.1414@gmail.com>
2021-04-24 03:59:35 +0900

    detach vuetify

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import vuetify from "./vuetify"
+//import vuetify from "./vuetify"
 import store from './store'
 
 Vue.config.productionTip = false
@@ -9,7 +9,7 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
-  vuetify,
+  //vuetify,
   render: h => h(App),
   created() {
     store.dispatch('loadAll')
